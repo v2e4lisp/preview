2192bcbf51b0a6d025bf0505e1ba3da81c170062
wenjun.yan <mylastnameisyan@gmail.com>
2013-11-06 20:55:58 +0900

    setup package.

--- real-assert.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class AssertionException extends Exception {}
-
-function realAssert($stmt, $message="failed.") {
-    if(!$stmt) {
-        throw new AssertionException($message);
-    }
-}
