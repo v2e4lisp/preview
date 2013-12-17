e4140da5afa50ab8f026b17060f8d90037e00f69
wenjun.yan <mylastnameisyan@gmail.com>
2013-12-18 00:18:55 +0900

    qunit and sample.

---
+++ sample/qunit_spec.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Preview\DSL\Qunit;
+
+require_once __DIR__ . '/../test/ok.php';
+
+suite("array_pop");
+
+// run before each test case;
+setup(function() {
+    $this->arr = array(1,2);
+});
+
+// use the variable set in the setup function
+test("return last element", function () {
+    ok(array_pop($this->arr) == 2);
+});
+
+// skip this case
+test("return null for empty string", function () {
+    ok(empty(array_pop(array())));
+})->skip();
+
+
+// test case without description;
+test(function() {
+    array_pop($this->arr);
+    ok(count($this->arr) == 1);
+});
+
+// pending test case
+test("array_pop a string?");
+
+// another suite;
+suite("array_push");
+
+// following test cases now belong to the "array_push" suite
+test("add one element to array", function () {
+    $target = array(1,2,3,4);
+    array_push($target, 5);
+    ok(count($this->arr) == 5);
+});
+
+
+
+
---
+++ src/preview/dsl/qunit.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Qunit like dsl
+ *
+ * @package Preview
+ * @author Wenjun Yan
+ * @email mylastnameisyan@gmail.com
+ */
+
+namespace Preview\DSL\Qunit;
+
+use \Preview\World;
+use \Preview\Core\TestSuite;
+use \Preview\Core\TestCase;
+
+/**
+ * start a test suite
+ *
+ * @param string $title
+ * @retrun object testsuite object.
+ */
+function suite($title) {
+    $desc = new TestSuite($title, function() {});
+    World::pop();
+    World::push($desc);
+    return $desc;
+}
+
+/**
+ * create a test case
+ *
+ * @param string $title
+ * @param function|null $fn
+ * @retrun object testcase object
+ */
+function test($title, $fn=null) {
+    if (is_callable($title)) {
+        $fn = $title;
+        $title = "";
+    }
+
+    $case = new TestCase($title, $fn);
+    $case->set_parent(World::current());
+    return $case;
+}
+
+/**
+ * A hook to be called before running each test case in current test suite.
+ *
+ * @param function $fn
+ * @retrun null
+ */
+function setup($fn) {
+    World::current()->before_each_hooks[] = $fn;
+}
+
+/**
+ * A hook to be called after running each test case in current test suite.
+ *
+ * @param string $param
+ * @retrun null
+ */
+function teardown($fn) {
+    World::current()->before_after_hooks[] = $fn;
+}
