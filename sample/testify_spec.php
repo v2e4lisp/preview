54b02fd9dbd562f8c2416ad6274ba480023263f1
wenjun.yan <mylastnameisyan@gmail.com>
2013-12-22 01:39:07 +0900

    testify dsl.

---
+++ sample/testify_spec.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Preview\DSL\Testify;
+
+require_once __DIR__ . '/../test/ok.php';
+
+$suite = new Suite("array functions");
+
+$suite->before_each(function () {
+    $this->arr = array(1,2,3,4);
+});
+
+$suite->test("array_push", function () {
+    array_push($this->arr, 1);
+    ok(end($this->arr) == 1);
+})->group("array");
+
+
---
+++ src/preview/dsl/testify.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Testify style dsl
+ *
+ * @package Preview
+ * @author Wenjun Yan
+ * @email mylastnameisyan@gmail.com
+ */
+
+namespace Preview\DSL\Testify;
+
+use \Preview\World;
+use \Preview\Core\TestSuite;
+use \Preview\Core\TestCase;
+
+class Suite {
+    private $suite;
+
+    public function __construct($title) {
+        $suite = new TestSuite($title, function(){});
+        World::pop();
+        World::push($suite);
+        $this->suite = $suite;
+    }
+
+    public function skip() {
+        $this->suite->skip();
+        return $this;
+    }
+
+    public function group() {
+        $groups = func_get_args();
+        foreach($groups as $group) {
+            $this->suite->group($group);
+        }
+        return $this;
+    }
+
+    public function before($fn) {
+        $this->suite->before($fn);
+        return $this;
+    }
+
+    public function after($fn) {
+        $this->suite->after($fn);
+        return $this;
+    }
+
+    public function before_each($fn) {
+        $this->suite->before_each($fn);
+        return $this;
+    }
+
+    public function after_each($fn) {
+        $this->suite->after_each($fn);
+        return $this;
+    }
+
+    public function test($title, $fn=null) {
+        if (empty($fn) and $title instanceof \Closure) {
+            $fn = $title;
+            $title = "";
+        }
+        $case = new TestCase($title, $fn);
+        $this->suite->add($case);
+        return $case;
+    }
+}
