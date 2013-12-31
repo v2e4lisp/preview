8ea1d4d0d42f0982d957be31e83c051416fcf48f
wenjun.yan <mylastnameisyan@gmail.com>
2013-12-31 18:10:59 +0900

    typo fix.

--- sample/error_spec.php
+++ sample/error_spec.php
@@ -8,6 +8,11 @@ describe("error", function () {
     it("should be converted to error_exception", function () {
         $a->value;
     });
+
+    it("should be ok", function () {
+        trigger_error("This event WILL fire", E_USER_NOTICE);
+        ok(true);
+    });
 });
 
 
