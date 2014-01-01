bad432cd656e8573ea2f0ca70e511335dfd574fc
wenjun.yan <mylastnameisyan@gmail.com>
2014-01-01 13:15:36 +0900

    bdd spec.

--- spec/dsl/bdd_spec.php
+++ spec/dsl/bdd_spec.php
@@ -17,30 +17,181 @@ describe("bdd", function () {
     before_each(function () {
         $this->world = new World;
         $this->config = new Configuration;
-        $this->config->reporter = new BaseReporter;
+        $this->config->reporter = new \Recorder;
     });
 
-    context("when no test groups specified", function () {
-        it("should run all the tests", function () {
+    describe("test structure", function () {
+        before_each(function () {
+            // start new env
+            Preview::$world = $this->world;
+            Preview::$config = $this->config;
+
+            // ------ test -------
+            describe("s1", function () {
+                it("c1", function () {});
+                it("c2", function () {})->skip();
+                it("c3");
+
+                describe("s2", function () {
+                    it("c4", function () {});
+                    it("c5", function () {})->skip();
+                    it("c6");
+                });
+            });
+            $this->results = $this->world->run();
+            // ------ end test -------
+
+            // end new env
+            // and go back to our normal test env
+            Preview::$world = $this->test_world;
+            Preview::$config = $this->test_config;
+        });
+
+        it("should have one child suite", function () {
+            ok(count($this->results) == 1);
+        });
+
+        it("should have 3 test cases", function ()  {
+            ok(count($this->results[0]->cases()) == 3);
+        });
+
+        it("child suite should contain 3 cases", function () {
+            $suites = $this->results[0]->suites();
+            ok(count($suites[0]->cases()) == 3);
+        });
+
+        it("child suite should contain no suite", function () {
+            $suites = $this->results[0]->suites();
+            ok(count($suites[0]->suites()) == 0);
+        });
+
+        describe("reporter hook", function () {
+            describe("#before_all", function () {
+                it("should be called once", function () {
+                    ok($this->config->reporter->before_all == 1);
+                });
+            });
+
+            describe("#after_all", function () {
+                it("should be called once", function () {
+                    ok($this->config->reporter->after_all == 1);
+                });
+            });
+
+            describe("#before_suite", function () {
+                it("should be called twice", function () {
+                    ok($this->config->reporter->before_suite == 2);
+                });
+            });
+
+            describe("#after_suite", function () {
+                it("should be called twice", function () {
+                    ok($this->config->reporter->after_suite == 2);
+                });
+            });
+
+            describe("#before_case", function () {
+                it("should be called 6 times", function () {
+                    ok($this->config->reporter->before_case == 6);
+                });
+            });
+
+            describe("#after_case", function () {
+                it("should be called 6 times", function () {
+                    ok($this->config->reporter->after_case == 6);
+                });
+            });
+        });
+    });
+
+    describe("test case state", function () {
+        before_each(function () {
             // start new env
             Preview::$world = $this->world;
             Preview::$config = $this->config;
 
             // below is our test
             describe("sample suite", function () {
-                it("sample test 1", function () {});
-                it("sample test 2", function () {});
+                it("error", function () {
+                    $a->value();
+                });
+
+                it("failed", function () {
+                    ok(false);
+                });
+
+                it("passed", function () {
+                });
+
+                it("pending");
+
+                it("skipped", function () {})->skip();
+
+                it("pending cannot skipped")->skip();
             });
-            $results = $this->world->run();
+
+            $this->results = $this->world->run();
 
             // end new env
             // and go back to our normal test env
             Preview::$world = $this->test_world;
             Preview::$config = $this->test_config;
+        });
 
-            // assert
-            ok(count($results) == 1);
-            ok(count($results[0]->all_cases()) == 2);
+        it("should have 1 testsuite result and 6 testcase", function () {
+            ok(count($this->results) == 1);
+            ok(count($this->results[0]->all_cases()) == 6);
+        });
+
+        it("should have one error test result", function () {
+            ok(function () {
+                foreach($this->results as $result) {
+                    if($result->error()) {
+                        return true;
+                    }
+                }
+            });
+        });
+
+        it("should have one failure test result", function () {
+            ok(function () {
+                foreach($this->results as $result) {
+                    if($result->failed()) {
+                        return true;
+                    }
+                }
+            });
+        });
+
+        it("should have one passed test result", function () {
+            ok(function () {
+                foreach($this->results as $result) {
+                    if($result->passed()) {
+                        return true;
+                    }
+                }
+            });
+        });
+
+        it("should have one skipped test result", function () {
+            ok(function () {
+                foreach($this->results as $result) {
+                    if($result->skipped()) {
+                        return true;
+                    }
+                }
+            });
+        });
+
+        it("should have two pending test results", function () {
+            ok(function () {
+                $total = 0;
+                foreach($this->results as $result) {
+                    if($result->pending()) {
+                        $total++;
+                    }
+                }
+            });
         });
     });
 });
--- spec/helper.php
+++ spec/helper.php
@@ -10,16 +10,36 @@ function ok($expr, $msg="") {
     }
 }
 
-class FakeTest {
-    public $result;
 
-    public function __construct($title) {
-        $this->result = new \stdClass;
-        $this->result->title = $title;
-        $this->result->run = false;
+class Recorder {
+    public $before_all = 0;
+    public $after_all = 0;
+    public $before_suite = 0;
+    public $after_suite = 0;
+    public $before_case = 0;
+    public $after_case = 0;
+
+    public function before_all($results) {
+        $this->before_all++;
     }
 
-    public function run() {
-        $this->result->run = true;
+    public function after_all($results) {
+        $this->after_all++;
+    }
+
+    public function before_suite($suite) {
+        $this->before_suite++;
+    }
+
+    public function after_suite($suite) {
+        $this->after_suite++;
+    }
+
+    public function before_case($case) {
+        $this->before_case++;
+    }
+
+    public function after_case($case) {
+        $this->after_case++;
     }
 }
