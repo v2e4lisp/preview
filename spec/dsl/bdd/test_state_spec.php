3e9fc22aaf29e11d2155771e9ff32bec555931d1
wenjun.yan <mylastnameisyan@gmail.com>
2014-01-02 21:26:15 +0900

    bdd test suite state.

--- spec/dsl/bdd/test_state_spec.php
+++ spec/dsl/bdd/test_state_spec.php
@@ -19,7 +19,69 @@ describe("bdd[test state]", function () {
         $this->config->reporter = new \Recorder;
     });
 
-    describe("sample", function () {
+    describe("suite state", function () {
+        before_each(function () {
+            // start new env
+            Preview::$world = $this->world;
+            Preview::$config = $this->config;
+
+            // below is our test
+            describe("sample suite", function () {});
+            describe("sample suite", function () {
+                it("failed", function () {
+                    ok(false);
+                });
+            });
+            describe("sample suite", function () {})->skip();
+            describe("sample suite");
+            describe("sample suite")->skip();
+
+            $this->results = $this->world->run();
+
+            // end new env
+            // and go back to our normal test env
+            Preview::$world = $this->test_world;
+            Preview::$config = $this->test_config;
+        });   
+
+        it("sample should have 5 results", function () {
+            ok(count($this->results) == 5);
+        });
+        
+        it("sample should have 1 passed suite", function () {
+            $all = $this->results;
+            $result = array_filter($all, function ($suite) {
+                return $suite->passed();
+            });
+            ok(count($result) == 1);
+        });
+
+        it("sample should have 1 failed suite", function () {
+            $all = $this->results;
+            $result = array_filter($all, function ($suite) {
+                return $suite->failed();
+            });
+            ok(count($result) == 1);
+        });
+        
+        it("sample should have 2 pending suite", function () {
+            $all = $this->results;
+            $result = array_filter($all, function ($suite) {
+                return $suite->pending();
+            });
+            ok(count($result) == 2);
+        });
+
+        it("sample should have 1 skipped suite", function () {
+            $all = $this->results;
+            $result = array_filter($all, function ($suite) {
+                return $suite->skipped();
+            });
+            ok(count($result) == 1);
+        });
+    });
+
+    describe("case state", function () {
         before_each(function () {
             // start new env
             Preview::$world = $this->world;
@@ -52,14 +114,13 @@ describe("bdd[test state]", function () {
             Preview::$world = $this->test_world;
             Preview::$config = $this->test_config;
         });
-
-
-        it("should have 1 testsuite result and 6 testcase", function () {
+        
+        it("sample should have 1 testsuite result and 6 testcase", function () {
             ok(count($this->results) == 1);
             ok(count($this->results[0]->all_cases()) == 6);
         });
 
-        it("should have one error test result", function () {
+        it("sample should have one error test result", function () {
             $all = $this->results[0]->all_cases();
             $result = array_filter($all, function ($case) {
                 return $case->error();
@@ -67,7 +128,7 @@ describe("bdd[test state]", function () {
             ok(count($result) == 1);
         });
 
-        it("should have one failure test result", function () {
+        it("sample should have one failure test result", function () {
             $all = $this->results[0]->all_cases();
             $result = array_filter($all, function ($case) {
                 return $case->failed();
@@ -75,7 +136,7 @@ describe("bdd[test state]", function () {
             ok(count($result) == 1);
         });
 
-        it("should have one passed test result", function () {
+        it("sample should have one passed test result", function () {
             $all = $this->results[0]->all_cases();
             $result = array_filter($all, function ($case) {
                 return $case->passed();
@@ -83,7 +144,7 @@ describe("bdd[test state]", function () {
             ok(count($result) == 1);
         });
 
-        it("should have one skipped test result", function () {
+        it("sample should have one skipped test result", function () {
             $all = $this->results[0]->all_cases();
             $result = array_filter($all, function ($case) {
                 return $case->skipped();
@@ -91,7 +152,7 @@ describe("bdd[test state]", function () {
             ok(count($result) == 1);
         });
 
-        it("should have two pending test results", function () {
+        it("sample should have two pending test results", function () {
             $all = $this->results[0]->all_cases();
             $result = array_filter($all, function ($case) {
                 return $case->pending();
