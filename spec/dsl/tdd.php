4000898b8543fe83d75134a7a56d94921a3f3bea
wenjun.yan <mylastnameisyan@gmail.com>
2014-01-02 21:53:54 +0900

    tdd spec.

---
+++ spec/dsl/tdd.php
@@ -0,0 +1,27 @@
+<?php
+
+describe("tdd", function () {
+    describe("#suite", function () {
+        it("alias for bdd#describe");
+    });
+
+    describe("#test", function () {
+        it("alias for bdd#it");
+    });
+
+    describe("#setup", function () {
+        it("alias for bdd#before_each");
+    });
+
+    describe("#suite_setup", function () {
+        it("alias for bdd#before");
+    });
+
+    describe("#teardown", function () {
+        it("alias for bdd#after_each");
+    });
+
+    describe("#suite_teardown", function () {
+        it("alias for bdd#after");
+    });
+});
