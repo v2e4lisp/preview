96f911e149219f79cbc7bb68cff1a524ca49ff16
wenjun.yan <mylastnameisyan@gmail.com>
2014-01-09 21:33:37 +0900

    comment export.

--- src/preview/dsl/export.php
+++ src/preview/dsl/export.php
@@ -14,7 +14,20 @@ use \Preview\Core\TestSuite;
 use \Preview\Core\TestCase;
 use \Preview\DSL\Util;
 
-function export ($title, $options) {
+/**
+ * Load an array of tests (a test suite)
+ *
+ * @param string|array $title, title of a test suite or an array of tests
+ * @param array|null $options, an array of tests
+ *        key for hooks: "before", "after", "before each", "after each",
+ *        other keys are for test cases.
+ */
+function export ($title, $options=null) {
+    if (is_null($options) and is_array($title)) {
+        $options = $title;
+        $title = "";
+    }
+
     $suite = new TestSuite($title, function() {});
     Util::set_default_filename_and_lineno($suite, debug_backtrace());
     Preview::$world->pop();
